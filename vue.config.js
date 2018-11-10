478f6d60201ad7da91f20ac482c38e2ae7b4c26a
James Rice <James.Rice@ttg-solutions.com>
2018-11-10 14:30:37 -0500

    Updated dist.

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,7 @@
 // vue.config.js
 
-console.log(process.env)
-
-// process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : '/
-// outputDir: process.env.VUE_APP_OUTPUT_DIR ? process.env.VUE_APP_OUTPUT_DIR : 'dist',
-// baseUrl: '/blah-blah'
-
 module.exports = {
     // options...
+    outputDir: process.env.VUE_APP_OUTPUT_DIR ? process.env.VUE_APP_OUTPUT_DIR : 'dist',
     baseUrl: process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : '/'
   }
